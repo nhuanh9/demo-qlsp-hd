1e45e2ceae127fcf3312810b18eb5d8161ddc8b8
nhuanh9 <chopperlouis@gmail.com>
2022-06-22 22:09:37 +0700

    reset

--- src/main/java/com/example/demo/serivce/post/IPostService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.serivce.post;
-
-import com.example.demo.model.Post;
-import com.example.demo.serivce.GeneralService;
-import org.springframework.data.repository.query.Param;
-
-public interface IPostService extends GeneralService<Post> {
-    Iterable<Post> findByTitleAndCreatedTime(String textSearch, String timeStart, String timeEnd);
-}
